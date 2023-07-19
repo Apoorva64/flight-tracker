ac91b2c80edefb752c9c191624d56ebd1a7232ed
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-07-19 21:40:00 +0200

    feat: remove template Items in drawer

--- src/components/drawer/DrawerItems.tsx
+++ src/components/drawer/DrawerItems.tsx
@@ -1,29 +1,14 @@
-import {Divider, List, ListItem, ListItemIcon, ListItemText} from "@mui/material";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+import {List} from "@mui/material";
 import {DataSourceFilter} from "./DataSourceFilter.tsx";
 import {AdvancedFilters} from "./AdvancedFilters.tsx";
 import {Options} from "./Options.tsx";
 
 export function DrawerItems() {
     return (
-        <>
-            <List>
-                <DataSourceFilter/>
-                <Options/>
-                <AdvancedFilters/>
-            </List>
-            <Divider/>
-            <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                    <ListItem key={text}>
-                        <ListItemIcon>
-                            {index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}
-                        </ListItemIcon>
-                        <ListItemText primary={text}/>
-                    </ListItem>
-                ))}
-            </List>
-        </>
+        <List>
+            <DataSourceFilter/>
+            <Options/>
+            <AdvancedFilters/>
+        </List>
     )
 }
